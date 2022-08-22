5b07ea3c63d697bc63ae5462ba245f0efb5dbe0a
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2022-08-22 17:08:11 +0100

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+//Weather API 
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let discriptionElemenet = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let feelsLike = document.querySelector("#feels-like");
+  let windSpeed = document.querySelector("#windSpeed");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  discriptionElemenet.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "ebc2487298b0e7c6cfcfbfe23bd11495";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+// Date/Time Section
 
