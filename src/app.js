fe94f48fe5a25f3e75ba45d13f814c4460a1dad3
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2022-08-22 17:33:55 +0100

    Added current time/date with JavaScript

--- src/app.js
+++ src/app.js
@@ -1,4 +1,45 @@
-//Weather API 
+// Date/Time Section
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[now.getDay()];
+  let date = now.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  return `${day} ${date} ${month} <br  />${hours}:${minutes}`;
+}
+
+//Weather API
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -7,18 +48,17 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let feelsLike = document.querySelector("#feels-like");
   let windSpeed = document.querySelector("#windSpeed");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   discriptionElemenet.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "ebc2487298b0e7c6cfcfbfe23bd11495";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
-
-// Date/Time Section
-
