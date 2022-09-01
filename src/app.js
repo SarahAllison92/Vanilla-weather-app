ad4836e6b4966adc63fb0fa9e2bcb8dfe9055584
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2022-09-01 11:03:26 +0100

    Added the relevant icon using API

--- src/app.js
+++ src/app.js
@@ -9,31 +9,22 @@ function formatDate(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
   let day = days[now.getDay()];
   let date = now.getDate();
   let months = [
-    "January",
-    "February",
-    "March",
-    "April",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
     "May",
     "June",
     "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
   ];
   let month = months[now.getMonth()];
   return `${day} ${date} ${month} <br  />${hours}:${minutes}`;
@@ -49,6 +40,8 @@ function displayTemperature(response) {
   let feelsLike = document.querySelector("#feels-like");
   let windSpeed = document.querySelector("#windSpeed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   discriptionElemenet.innerHTML = response.data.weather[0].description;
@@ -56,9 +49,15 @@ function displayTemperature(response) {
   feelsLike.innerHTML = Math.round(response.data.main.feels_like);
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "ebc2487298b0e7c6cfcfbfe23bd11495";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+let city = "Berlin";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
